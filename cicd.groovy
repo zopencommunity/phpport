c664f62cfb942149ed57e7ab4966209d8a97fcca
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:08 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/phpport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/phpport.git'), string(name: 'PORT_DESCRIPTION', value: 'PHP (recursive acronym for PHP: Hypertext Preprocessor) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML.' )]
   }
